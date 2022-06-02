b657ad2bfce269cd4df2b94a0b99f8407ce70140
vm-php <marcinxmurach995@gmail.com>
2022-06-02 12:46:29 +0200

    :boom: Use redis instead of cache, make keys more readable

--- app/Cache/MessageCacheService.php
+++ app/Cache/MessageCacheService.php
@@ -4,23 +4,42 @@ declare(strict_types=1);
 
 namespace App\Cache;
 
+use App\Discord\AuthorValidator;
 use Discord\Parts\Channel\Message;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Redis\Connections\Connection;
+use Illuminate\Support\Facades\Redis;
+use Webmozart\Assert\Assert;
 
 class MessageCacheService implements CacheServiceInterface
 {
-    private const USER_CHANNEL_MESSAGE_KEY_PATTERN = 'ChannelUserMessage_%s_%s_%s';
+    private const USERS_CHANNEL_MESSAGE_KEY_PATTERN = 'ChannelsMessages:%s:Users:%s';
+    private const BOTS_CHANNEL_MESSAGE_KEY_PATTERN = 'ChannelsMessages:%s:Bots:%s';
+
+    /** @var Connection */
+    private $connection;
+
+    /** @var AuthorValidator */
+    private $authorValidator;
+
+    public function __construct()
+    {
+        $this->connection = Redis::connection();
+        $this->authorValidator = new AuthorValidator();
+    }
 
     public function store(Message $message): void
     {
-        Cache::forever(
-            sprintf(
-                self::USER_CHANNEL_MESSAGE_KEY_PATTERN,
-                $message->channel_id,
-                $message->user_id,
-                $message->id
-            ),
-            (string) $message
+        $this->connection->client()->rPush($this->key($message), (string) $message);
+    }
+
+    private function key(Message $message): string
+    {
+        return sprintf(
+            $this->authorValidator->isBot($message->author)
+                ? self::BOTS_CHANNEL_MESSAGE_KEY_PATTERN
+                : self::USERS_CHANNEL_MESSAGE_KEY_PATTERN,
+            $message->channel_id,
+            $message->user_id
         );
     }
 }
